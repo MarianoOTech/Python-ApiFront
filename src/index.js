8ed6db903651716f293f732fed8bb69dd2ca5d0f
MarianoOTech <marianosanro23@gmail.com>
2024-05-17 12:00:33 -0600

    docker

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // 2
 const httpLink = createHttpLink({
-  uri: 'http://localhost:8000/graphql/'
+  uri: 'http://34.145.24.231:8080/graphql/'
 });
 
 const authLink = setContext((_, { headers }) => {
